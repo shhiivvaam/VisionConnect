f57803efb20320057bc6220ddc12e38fb900ab0c
Shivam Kumar <life.shivam2394@gmail.com>
2024-02-21 13:18:28 +0530

    Added the Socket Configuration and Connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
+import LobbyScreen from './screens/Lobby';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="text-red-500">
-      Shivam
+    <div className="App bg-black text-white h-screen w-full flex justify-center items-center">
+      <Routes>
+        <Route path='/' element={<LobbyScreen />} />
+      </Routes>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter } from 'react-router-dom';
+import { SocketProvider } from './context/SocketProvider';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <SocketProvider>
+        <App />
+      </SocketProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
